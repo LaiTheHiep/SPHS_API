7bfd37bbf9298d5c3ea847d766d263b0254b502d
Hiep Lai <laihiep02121997@gmail.com>
2020-07-13 15:00:53 +0700

    regex user

--- api/users.api.js
+++ api/users.api.js
@@ -31,6 +31,7 @@ module.exports = {
         var query = req.query;
         var _skip = env.skip;
         var _limit = env.limit;
+        var _sort = { ...env.sort };
         delete query.accessToken;
         if (query['$skip']) {
           _skip = parseInt(query['$skip']);
@@ -39,6 +40,13 @@ module.exports = {
         if (query['$limit']) {
           _limit = parseInt(query['$limit']);
           delete query['$limit'];
+        }
+        if (query['$sort']) {
+          let _temp_sort = query['$sort'].trim().substring(1, query['$sort'].trim().length - 1);
+          let _arr_sort = _temp_sort.split(':');
+          _sort = {};
+          _sort[_arr_sort[0].trim()] = parseInt(_arr_sort[1].trim())
+          delete query['$sort'];
         }
         if (query['$regex']) {
           let regex_arr = JSON.parse(query['$regex']);
@@ -50,17 +58,7 @@ module.exports = {
           });
           delete query['$regex'];
         }
-        objectSchema.find(query).skip(_skip).limit(_limit).exec((err, data) => {
-          if (err) {
-            res.send({
-              total: 0,
-              data: [],
-              errorName: err.name,
-              errorMessage: err.message
-            });
-            return;
-          }
-
+        objectSchema.find(query).skip(_skip).limit(_limit).sort({ ..._sort }).then((data) => {
           for (let i = 0; i < data.length; i++) {
             data[i].password = undefined;
           }
